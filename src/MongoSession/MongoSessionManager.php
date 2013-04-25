67715bfe08c58eabbee885c3b6333b02159dcf3f
Mustafa Navruz <mustafanavruz@gmail.com>
2013-04-25 23:38:02 +0300

    Use Symfony HttpFoundation MongoDbSessionHandler. Fixes #1

--- src/MongoSession/MongoSessionManager.php
+++ src/MongoSession/MongoSessionManager.php
@@ -1,33 +1,46 @@
 <?php namespace MongoSession;
 
 use Illuminate\Session\SessionManager;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\MongoDbSessionHandler;
 
 class MongoSessionManager extends SessionManager {
 
 	/**
 	 * Create an instance of the database session driver.
 	 *
-	 * @return MongoSession\MongoStore
+	 * @return \Illuminate\Session\Store
 	 */
 	protected function createMongoDriver()
 	{
-		$connection = $this->getMongoConnection();
+		$connection = $this->getMongoDbConnection();
 
 		$collection = $this->app['config']['session.table'];
 
-		return new MongoStore($connection, $this->app['encrypter'], $collection);
+		$database = (string) $connection->getMongoDB();
+
+		return $this->buildSession(new MongoDbSessionHandler($connection->getMongoClient(), $this->getMongoDbOptions($database, $collection)));
 	}
 
 	/**
-	 * Get the database connection for the database driver.
+	 * Get the database connection for the MongoDB driver.
 	 *
 	 * @return LMongo\Connection
 	 */
-	protected function getMongoConnection()
+	protected function getMongoDbConnection()
 	{
 		$connection = $this->app['config']['session.connection'];
 
 		return $this->app['lmongo']->connection($connection);
+	}
+
+	/**
+	 * Get the database session options.
+	 *
+	 * @return array
+	 */
+	protected function getMongoDbOptions($database, $collection)
+	{
+		return array('database' => $database,'collection' => $collection, 'id_field' => '_id', 'data_field' => 'payload', 'time_field' => 'last_activity');
 	}
 
 }
--- src/MongoSession/SessionServiceProvider.php
+++ src/MongoSession/SessionServiceProvider.php
@@ -1,30 +1,8 @@
 <?php namespace MongoSession;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Session\SessionServiceProvider as ServiceProvider;
 
 class SessionServiceProvider extends ServiceProvider {
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->registerSessionEvents();
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->registerSessionManager();
-
-		$this->registerSessionDriver();
-	}
 
 	/**
 	 * Register the session manager instance.
@@ -37,118 +15,6 @@ class SessionServiceProvider extends ServiceProvider {
 		{
 			return new MongoSessionManager($app);
 		});
-	}
-
-	/**
-	 * Register the session driver instance.
-	 *
-	 * @return void
-	 */
-	protected function registerSessionDriver()
-	{
-		$this->app['session'] = $this->app->share(function($app)
-		{
-			// First, we will create the session manager which is responsible for the
-			// creation of the various session drivers when they are needed by the
-			// application instance, and will resolve them on a lazy load basis.
-			$manager = $app['session.manager'];
-
-			$driver = $manager->driver();
-
-			$config = $app['config']['session'];
-
-			// Once we get an instance of the session driver, we need to set a few of
-			// the session options based on the application configuration, such as
-			// the session lifetime and the sweeper lottery configuration value.
-			$driver->setLifetime($config['lifetime']);
-
-			$driver->setSweepLottery($config['lottery']);
-
-			return $driver;
-		});
-	}
-
-	/**
-	 * Register the events needed for session management.
-	 *
-	 * @return void
-	 */
-	protected function registerSessionEvents()
-	{
-		$app = $this->app;
-
-		$config = $app['config']['session'];
-
-		// The session needs to be started and closed, so we will register a before
-		// and after events to do all stuff for us. This will manage the loading
-		// the session "payloads", as well as writing them after each request.
-		if ( ! is_null($config['driver']))
-		{
-			$this->registerBootingEvent($app);
-
-			$this->registerCloseEvent($app, $config);
-		}
-	}
-
-	/**
-	 * Register the session booting event.
-	 *
-	 * @param  Illuminate\Foundation\Application  $app
-	 * @return void
-	 */
-	protected function registerBootingEvent($app)
-	{
-		$app->booting(function($app)
-		{
-			$app['session']->start($app['cookie'], $app['config']['session.cookie']);
-		});
-	}
-
-	/**
-	 * Register the session close event.
-	 *
-	 * @param  Illuminate\Foundation\Application  $app
-	 * @param  array $config
-	 * @return void
-	 */
-	protected function registerCloseEvent($app, $config)
-	{
-		$me = $this;
-
-		$app->close(function($request, $response) use ($me, $app, $config)
-		{
-			$session = $app['session'];
-
-			// Once we finish the session handling for the request, we will need to get a
-			// cookie that we can attach to the response from the application. This is
-			// used to identify the session on future requests into the application.
-			$session->finish($response, $config['lifetime']);
-
-			$cookie = $me->makeCookie($session, $config);
-
-			if ( ! is_null($cookie))
-			{
-				$response->headers->setCookie($cookie);
-			}
-		});
-	}
-
-	/**
-	 * Create a session cookie based on the given config.
-	 *
-	 * @param  Illuminate\Session\Store  $session
-	 * @param  array  $config
-	 * @return Symfony\Component\HttpFoundation\Cookie
-	 */
-	public function makeCookie($session, $config)
-	{
-		$app = $this->app;
-
-		return $session->getCookie(
-
-			$app['cookie'], $config['cookie'], $config['lifetime'], $config['path'], $config['domain']
-
-		);
 	}
 
 }
